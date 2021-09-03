5d98f5848d398fdcd0f30ed85b0768ac285d72cc
whskrdns <whskrdns@ubware.com>
2021-09-03 09:33:08 +0900

    commit

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(port);
 
   console.log(`listening to ${port} port`);
--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,12 +1,20 @@
-import { Args, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
 import { Restaurant } from './entities/restaurant.entity';
 
 @Resolver(() => Restaurant)
 export class RestaurantResolver {
   @Query(() => [Restaurant])
   restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
-    console.log(veganOnly);
+    console.log('veganOnly', veganOnly);
 
     return [];
+  }
+
+  @Mutation(() => Boolean)
+  createRestaurant(@Args() createRestaurant: CreateRestaurantDto): boolean {
+    console.log(createRestaurant);
+
+    return true;
   }
 }
