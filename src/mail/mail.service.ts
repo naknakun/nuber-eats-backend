6e3edd4ad562c6da347c6efdafce0086097ad6dc
whskrdns <whskrdns@ubcare.co.kr>
2022-09-19 09:29:05 +0900

    add node-fetch

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,37 +1,40 @@
-import got from 'got';
-import FormData from 'form-data';
 import { Inject, Injectable } from '@nestjs/common';
 import { CONFIG_OPTIONS } from 'src/common/common.constants';
 import { MailModuleOptions } from './mail.interfaces';
+import fetch from 'node-fetch';
 
 @Injectable()
 export class MailService {
   constructor(
     @Inject(CONFIG_OPTIONS) private readonly options: MailModuleOptions,
   ) {
-    this.sendEmail('testing', 'test');
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+    this.sendEmail('please', 'hahaha');
   }
 
   private async sendEmail(subject: string, content: string) {
-    const form = new FormData();
-    form.append('from', `Excited User <mailgun@${this.options.domain}>`);
-    form.append('to', 'whskrdns@gmail.com');
-    form.append('subject', subject);
-    form.append('text', content);
+    const params = new URLSearchParams();
+    params.append(
+      'from',
+      'Excited User <mailgun@sandboxc2ba12643e7349baa0bd6e741ff8ad84.mailgun.org>',
+    );
+    params.append('to', 'whskrdns@gmail.com');
+    params.append('subject', subject);
+    params.append('text', content);
 
-    const response = await got(
-      `https://api.mailgun.net/v3/${this.options.domain}/messages`,
+    const response = await fetch(
+      'https://api.mailgun.net/v3/sandboxc2ba12643e7349baa0bd6e741ff8ad84.mailgun.org/messages',
       {
+        method: 'POST',
+        body: params,
         headers: {
           Authorization: `Basic ${Buffer.from(
             `api:${this.options.apiKey}`,
           ).toString('base64')}`,
+          'Content-Type': 'application/x-www-form-urlencoded',
         },
-        method: 'POST',
-        body: form,
       },
     );
-
-    console.log(response.body);
+    console.log(await response.json());
   }
 }
